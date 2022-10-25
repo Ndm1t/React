e5c9a39cbac237b97d493e51bc367b962eab49d2
Nikita Dmytriienko <profinisher132@gmail.com>
2022-10-25 21:42:57 +0300

    New components + routs

--- src/components/DishCard.js
+++ src/components/DishCard.js
@@ -1,41 +1,52 @@
-import React,{Component} from "react";
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
+import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap'
 
-class DishCard extends Component{
-    constructor(props){
-        super(props)
-    }
 
-    render(){
-        const comments = this.props.dish.comments.map((comment) =>{
-            return (
-             <div key = {comment.id}>
-                <p>{comment.comment}</p>
-                <p>{comment.author} , {comment.date}</p>
-            </div>
-            )
-        })
+
+    
+    function RenderDish({dish}){
         return(
-            <div className="row">
-            <div className="col-12 col-md-5 m-1"> 
-            <Card>
-                    <CardImg width="100%"src={this.props.dish.image} alt={this.props.dish.name}/>
-                    <CardBody>
-                        <CardTitle>{this.props.dish.name}</CardTitle>
-                        <CardText>{this.props.dish.description}</CardText>
-                    </CardBody>
-            </Card>
-            </div>  
-                <div className="col-12 col-md-5 m-1">
-                    <h4>Comments</h4>
-                    {comments}
-                </div>
-            </div>
-            
+                    <Card className="col-12 col-md-5 m-1">
+                        <CardImg width="100%"src={dish.image} alt={dish.name}/>
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
         )
     }
 
-
-}
+    function RenderComments({comments}){
+        const cmnts = comments.map((comment) =>{
+            return (
+             <div key = {comment.id} >
+                <p>{comment.comment}</p>
+                <p>--{comment.author} , {new Intl.DateTimeFormat('en-US', {year: "numeric", month:"short", day:"2-digit"}).format(new Date(Date.parse(comment.date)))}</p>
+            </div>
+            )})
+        return(
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4> 
+                {cmnts}
+            </div>
+        )
+    }
+    
+    const DishCard = (props) =>{
+        if(props.dish)
+        {return(
+            <div className="container">
+                <div className="row">
+                  <RenderDish dish = {props.dish}/>
+                  <RenderComments comments = {props.dish.comments} />
+                </div>
+            </div>
+        )}
+        else{
+            return (
+                <div></div>
+            )
+        }
+    }
 
 export default DishCard 
---
+++ src/components/MainComponent.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Header from './HeaderComponent';
+import Menu from './MenuComponent';
+import DishCard from './DishCard';
+import Footer from './FooterComponent';
+import {DISHES}  from '../shared/dishes';
+import Home from './HomeComponent';
+import {Switch, Route, Redirect} from 'react-router-dom'
+
+class Main extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        dishes: DISHES,
+    };
+  }
+
+
+  render() {
+    const HomePage = () =>{
+      return(
+          <Home />
+      )
+    }
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path='/menu' component={() => <Menu dishes = {this.state.dishes}/>} />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
+      </div>  
+    );
+  }
+}
+
+export default Main;
