0364d27bc57dfb483c7e9cfb5dd0202d1d072e44
Nikita Dmytriienko <profinisher132@gmail.com>
2022-10-27 22:27:12 +0300

    Adding controlled an uncontrolled forms + validation + abot route

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,18 +1,30 @@
 import React, {Component} from "react";
-import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Button, Modal, ModalBody, ModalHeader, Form, FormGroup, Label, Input } from 'reactstrap';
 import { NavLink } from "react-router-dom";
 class Header extends Component{
     constructor(props){
         super(props)
 
         this.state = {
-            isNavOpen: false
+            isNavOpen: false,
+            isModalOpen : false
         }
         this.toggleNav = this.toggleNav.bind(this)
+        this.toggleModal = this.toggleModal.bind(this)
+        this.handleLogin = this.handleLogin.bind(this)
     }
 
     toggleNav(){
         this.setState({isNavOpen : !this.state.isNavOpen})
+    }
+    toggleModal(){
+        this.setState({isModalOpen : !this.state.isModalOpen})
+    }
+    handleLogin(event){
+        this.toggleModal()
+        alert("Username: " + this.username.value + " Password: " + this.password.value + " Remember: " + this.rem.checked)
+        event.preventDefault()
+
     }
     render(){
         return(
@@ -37,6 +49,15 @@ class Header extends Component{
                                 <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
                             </NavItem>
                             </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button 
+                                    onClick={this.toggleModal}
+                                    outline>
+                                        <span className="fa fa-sign-in fa-lg"> Log in</span>
+                                    </Button>
+                                </NavItem>
+                            </Nav>
                         </Collapse>
                         </div>
                     </div>
@@ -52,6 +73,33 @@ class Header extends Component{
                     </div>
                 </div>
             </div>
+            <Modal isOpen= {this.state.isModalOpen}
+                    toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal}> Log in </ModalHeader>
+                <ModalBody>
+                    <Form onSubmit={this.handleLogin}>
+                        <FormGroup>
+                            <Label htmlFor="username">Username</Label>
+                            <Input type="text" id="username" name="username" 
+                                innerRef = {(input)=> this.username = input}/>
+                        </FormGroup>
+
+                        <FormGroup>
+                            <Label htmlFor="password">Password</Label>
+                            <Input type="password" id="password" name="password"
+                            innerRef = {(input)=> this.password = input} />
+                        </FormGroup>
+                        <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="rem"
+                                    innerRef={(input) => this.rem = input} />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                        <Button type="submit" value="submit" className="primary">Log In</Button>
+                    </Form>
+                </ModalBody>
+            </Modal>
             </>
         )
     }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,7 +4,12 @@ import Menu from './MenuComponent';
 import DishCard from './DishCard';
 import Footer from './FooterComponent';
 import {DISHES}  from '../shared/dishes';
+import {COMMENTS}  from '../shared/comments';
+import {PROMOTIONS}  from '../shared/promotion';
+import {LEADERS}  from '../shared/leaders';
+import About from './AboutComponent';
 import Home from './HomeComponent';
+import Contact from './ContactConponent';
 import {Switch, Route, Redirect} from 'react-router-dom'
 
 class Main extends Component {
@@ -13,22 +18,34 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
+        comments : COMMENTS,
+        promotions : PROMOTIONS,
+        leaders : LEADERS
     };
   }
 
 
+
   render() {
-    const HomePage = () =>{
+    const DishWithId = ({match}) => {
       return(
-          <Home />
+        <DishCard dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId))[0]} 
+          comments = {this.state.comments.filter((com)=> com.dishId === parseInt(match.params.dishId))}
+        />
       )
     }
     return (
       <div>
         <Header />
         <Switch>
-          <Route path="/home" component={HomePage} />
+          <Route path="/home" component={() => <Home dish = {this.state.dishes.filter((dish)=> dish.featured)[0]} 
+            promotion = {this.state.promotions.filter((promo)=> promo.featured)[0]}
+            leader = {this.state.leaders.filter((lead)=> lead.featured)[0]}
+          />} />
           <Route exact path='/menu' component={() => <Menu dishes = {this.state.dishes}/>} />
+          <Route path='/menu/:dishId' component={DishWithId} />
+          <Route exact path="/contactus" component={Contact} />
+          <Route path='/aboutus' component={() => <About leaders = {this.state.leaders}/> } />
           <Redirect to="/home" />
         </Switch>
         <Footer />
